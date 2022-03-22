3b22a5bf75032a50f82506003fa2565bc95822de
arogya <arogyabichpuria16@gmail.com>
2022-03-22 18:28:48 +0530

    added a few providers to pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import {ChakraProvider} from "@chakra-ui/react";
+import theme from '../lib/themes';
+import CurrentTab from '../components/providers/CurrentTabProvider';
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+    <CurrentTab>
     <Component {...pageProps} />
+    </CurrentTab>
     </ChakraProvider>
   )
 }
